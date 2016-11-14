385cac227b05cd59691bb87263718993aa1f53a0
buiseb <bui.sebastien^gmail.com>
2016-11-14 22:55:20 +0100

    desactivate  db creation script and activate db creation in docker compose

--- sql/00-create-database.sql
+++ sql/00-create-database.sql
@@ -1,5 +1,6 @@
-DROP DATABASE IF EXISTS newmips;
-CREATE DATABASE newmips;
+#DROP DATABASE IF EXISTS newmips;
+#CREATE DATABASE newmips;
 
-CREATE USER 'newmips'@'127.0.0.1' IDENTIFIED BY 'newmips';
-GRANT ALL PRIVILEGES ON newmips.* TO 'newmips'@'127.0.0.1';
+#CREATE USER 'newmips'@'%' IDENTIFIED BY 'newmips';
+#GRANT ALL PRIVILEGES ON newmips.* TO 'newmips'@'%';
+#FLUSH PRIVILEGES;
