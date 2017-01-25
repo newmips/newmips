fa617afa9b2aaf6d4e26be60743eefd8bcb4517d
yirou <boyirou@gmail.com>
2017-01-25 12:01:09 +0100

    Added Mocha Testing Routine

--- test/common.js
+++ test/common.js
@@ -2,21 +2,11 @@
  * Common test script
  */
 
-var request = require("request"),
-        assert = require('assert'),
-        global = require('../config/global');
 
 
-describe("  Newmips Test ", function () {
-    var base_url = global.protocol + '://' + global.host + ':' + global.port;
-    
-    describe("GET /", function () {
-        it("returns status code 200", function () {
-            request.get(base_url, function (error, response, body) {
-                assert.equal(200, response.statusCode);
-                done();
-            });
-        });
+describe("***************************  Newmips Test *************************", function () {
+    it("Start", function () {
 
     });
+
 });
