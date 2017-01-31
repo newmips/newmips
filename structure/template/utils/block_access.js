83813862d375a6bfe77c079c0d87fc8af084fe57
Jerome Bonini <jbonini@innovantic.fr>
2017-01-31 11:43:18 +0100

    - session managment improved for applications - unique fields added to user/role/group - error check for unique field added to all controllers - autologin improved, now realy logedin as admin - user informations in top right corner re-activated

--- structure/template/utils/block_access.js
+++ structure/template/utils/block_access.js
@@ -5,12 +5,14 @@ var dbconfig = require('../config/database');
 exports.isLoggedIn = function(req, res, next) {
 	// Autologin for newmips's "iframe" live preview context
 	if (req.session.autologin == true)
+		models.E_user.findById(1).then(function(user) {
+			req.session.passport = {user: user};
+			return next();
+		});
+	else if (req.isAuthenticated())
 		return next();
-
-	if (req.isAuthenticated()) {
-		return next();
-	}
-	res.redirect('/login');
+	else
+		res.redirect('/login');
 };
 
 //If the user is already identified, he can't access the login page
