8fd5e1dadb3bf6b2b887bc40649d9006027802c4
Jerome Bonini <jbonini@innovantic.fr>
2017-03-16 13:52:25 +0100

    - instruction "create component cra" added - cra module creation working

--- structure/template/public/js/newmips.js
+++ structure/template/public/js/newmips.js
@@ -157,9 +157,9 @@ $(document).ready(function () {
     $('td[data-type="boolean"]').each(function () {
         var val = $(this).html();
         if (val == 'true' || val == '1')
-            $(this).html('<i class="fa fa-check-square-o fa-lg"></i>"');
+            $(this).html('<i class="fa fa-check-square-o fa-lg"></i>');
         else
-            $(this).html('<i class="fa fa-square-o fa-lg"></i>"');
+            $(this).html('<i class="fa fa-square-o fa-lg"></i>');
     });
 
     /* After good format -> Date / Datetime instanciation */
