0d0ff107cb38b7ba86a529b50f6ef354bda5ed4e
Jerome Bonini <jbonini@innovantic.fr>
2017-04-13 17:35:42 +0200

    - everything working

--- routes/ui_editor.js
+++ routes/ui_editor.js
@@ -18,13 +18,57 @@ var models = require('../models/');
 router.get('/getPage/:entity/:page', block_access.isLoggedIn, function(req, res) {
 	var page = req.params.page;
 	var entity = req.params.entity;
+	var generatorLanguage = language(req.session.lang_user);
+	var workspaceLanguage = require(__dirname+'/../workspace/'+req.session.id_application+'/services/language')(req.session.lang_user);
 
 	var pageUri = __dirname+'/../workspace/'+req.session.id_application+'/views/'+entity+'/'+page;
 	domHelper.read(pageUri).then(function($) {
+		// Encapsulate traduction with span to be able to translate, keep comment for later use
+		var tradRegex = new RegExp(/(<!--{@__ key="(.*)" ?\/}-->)/g);
+		$("body #fields")[0].innerHTML = $("body #fields")[0].innerHTML.replace(tradRegex, '<span class="trad-result">$2</span><span class="trad-src">$1</span>');
+
+		// Translate each .trad-result
+		$(".trad-result").each(function() {
+			var tradKey = $(this).text();
+			$(this).text(workspaceLanguage.__(tradKey));
+		});
+
+		// Hide action buttons
+		$(".actions").hide();
+
 		res.status(200).send($("body")[0].innerHTML);
 	}).catch(function(err) {
 		console.log(err);
-		res.status(404).send(language(req.session.lang_user)("ui_editor.page_not_fouond"));
+		res.status(404).send(generatorLanguage.__("ui_editor.page_not_found"));
+	});
+});
+
+router.post('/setPage/:entity/:page', block_access.isLoggedIn, function(req, res) {
+	var page = req.params.page;
+	var entity = req.params.entity;
+	var html = req.body.html;
+	var generatorLanguage = language(req.session.lang_user);
+
+	var pageUri = __dirname+'/../workspace/'+req.session.id_application+'/views/'+entity+'/'+page;
+	domHelper.loadFromHtml(html).then(function($) {
+		// Remove "forced" traduction
+		$(".trad-result").remove();
+
+		// Pull dust traduction out of .trad-src span
+		$(".trad-src").each(function() {
+			$(this).replaceWith($(this).html());
+		});
+
+		// Show action buttons
+		$(".actions").show();
+
+		// Write back to file
+		domHelper.write(pageUri, $).then(function() {
+			res.status(200).send(generatorLanguage.__("ui_editor.page_saved"));
+		});
+	}).catch(function(e) {
+		console.log(e);
+		res.status(500).send(generatorLanguage.__("ui_editor.page_not_found"));
 	});
 });
 
