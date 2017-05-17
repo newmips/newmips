92e3c2a00a317e1fafea0866b516a91493d5dc11
SnafuB <steph63b@hotmail.fr>
2017-05-17 09:17:49 +0200

    add slack config file in config

---
+++ config/slack.js
@@ -0,0 +1,21 @@
+var globalConf = require('./global');
+
+var slack = {
+	develop: {
+		'SLACK_API_URL': 'https://slack.com/api/channels.join',
+		'SLACK_API_INVITE_URL': 'https://slack.com/api/channels.invite',
+		'SLACK_API_USER_TOKEN': ''
+	},
+	recette: {
+		'SLACK_API_URL': 'https://slack.com/api/channels.join',
+		'SLACK_API_INVITE_URL': 'https://slack.com/api/channels.invite',
+		'SLACK_API_USER_TOKEN': ''
+	},
+	production: {
+		'SLACK_API_URL': 'https://slack.com/api/channels.join',
+		'SLACK_API_INVITE_URL': 'https://slack.com/api/channels.invite',
+		'SLACK_API_USER_TOKEN': ''
+	},
+}
+
+module.exports = slack[globalConf.env];
