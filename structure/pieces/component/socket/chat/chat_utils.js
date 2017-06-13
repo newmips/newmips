d286ca2d07a6769b157dbeaae1a6b8b3bd7fa26e
Jerome Bonini <jerome.bonini@newmips.com>
2017-06-13 16:59:21 +0200

    - channel invite/join modified

--- structure/pieces/component/socket/chat/chat_utils.js
+++ structure/pieces/component/socket/chat/chat_utils.js
@@ -174,7 +174,7 @@ exports.bindSocket = function(user, socket, connectedUsers) {
 				id: parseInt(data.id_channel)
 			}).then(function(channel) {
 				models.E_user.findById(user.id).then(function(userObj) {
-					userObj.addR_user_channel(channel).then(function() {
+					userObj.addR_user_channel(channel.id).then(function() {
 						// Refresh contact list
 						sendChatChannelList(user, socket);
 					});
@@ -190,7 +190,7 @@ exports.bindSocket = function(user, socket, connectedUsers) {
 				id: parseInt(data.id_channel)
 			}).then(function(channel) {
 				models.E_user.findById(parseInt(data.id_user)).then(function(userObj) {
-					userObj.addR_user_channel(channel).then(function() {
+					userObj.addR_user_channel(channel.id).then(function() {
 						// Refresh contact list
 						sendChatChannelList(user, socket);
 						if (connectedUsers[data.id_user])
