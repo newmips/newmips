85df1d586ae307cfa20b7663950106438b9f6e9b
SnafuB <steph63b@hotmail.fr>
2017-10-18 11:18:18 +0200

    windows 1252 encoding is now allowed for script

--- routes/instruction_script.js
+++ routes/instruction_script.js
@@ -201,7 +201,7 @@ router.post('/execute', block_access.isLoggedIn, multer({
     var fileContent = fs.readFileSync(req.file.path);
     var encoding = require('jschardet').detect(fileContent);
     // If extension or encoding is not supported, send error
-    if ((extensionFile != 'txt' && extensionFile != 'nps') || (encoding.encoding.toLowerCase() != 'utf-8' && encoding.encoding.toLowerCase() != 'ascii')) {
+    if ((extensionFile != 'txt' && extensionFile != 'nps') || (encoding.encoding.toLowerCase() != 'utf-8' && encoding.encoding.toLowerCase() != 'windows-1252' && encoding.encoding.toLowerCase() != 'ascii')) {
         scriptData[userId].answers.push({
             message: "File need to have .nps or .txt extension and utf8 or ascii encoding.<br>Your file have '"+extensionFile+"' extension and '"+encoding.encoding+"' encoding"
         });
