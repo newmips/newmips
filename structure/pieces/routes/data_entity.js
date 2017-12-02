85a638578536d18f0111863c2ad60de4871bca1e
Jerome Bonini <jerome.bonini@newmips.com>
2017-12-02 18:42:01 +0100

    - status displayed in source entity list_fields 	- /datalist modified to fetch current status and translate each 	  row

--- structure/pieces/routes/data_entity.js
+++ structure/pieces/routes/data_entity.js
@@ -36,60 +36,68 @@ router.post('/datalist', block_access.actionAccessMiddleware("ENTITY_URL_NAME", 
     /* Looking for include to get all associated related to data for the datalist ajax loading */
     var include = model_builder.getDatalistInclude(models, options);
     filterDataTable("MODEL_NAME", req.body, include).then(function (data) {
-        // Replace data enum value by translated value for datalist
-        var enumsTranslation = enums_radios.translated("ENTITY_NAME", req.session.lang_user, options);
-        var todo = [];
-        for (var i = 0; i < data.data.length; i++) {
-            for (var field in data.data[i].dataValues) {
-                // Look for enum translation
-                for (var enumEntity in enumsTranslation)
-                    for (var enumField in enumsTranslation[enumEntity])
-                        if (enumField == field)
-                            for (var j = 0; j < enumsTranslation[enumEntity][enumField].length; j++)
-                                if (enumsTranslation[enumEntity][enumField][j].value == data.data[i].dataValues[field]) {
-                                    data.data[i].dataValues[field] = enumsTranslation[enumEntity][enumField][j].translation;
-                                    break;
-                                }
 
-                //get attribute value
-                var value = data.data[i].dataValues[field];
-                //for type picture, get thumbnail picture
-                if (typeof attributes[field] != 'undefined' && attributes[field].newmipsType == 'picture' && value != null) {
-                    var partOfFile = value.split('-');
-                    if (partOfFile.length > 1) {
-                        //if field value have valide picture name, add new task in todo list
-                        //we will use todo list to get all pictures binary
-                        var thumbnailFolder = global.thumbnail.folder;
-                        var filePath = thumbnailFolder + 'ENTITY_NAME/' + partOfFile[0] + '/' + value;
-                        todo.push({
-                            value: value,
-                            file: filePath,
-                            field: field,
-                            dataIndex: i
-                        });
+        var statusPromises = [];
+        if (entity_helper.status.statusFieldList(attributes).length > 0)
+            for (var i = 0; i < data.data.length; i++)
+                statusPromises.push(entity_helper.status.currentStatus(models, "ENTITY_NAME", data.data[i], attributes, req.session.lang_user));
+
+        Promise.all(statusPromises).then(function() {
+            // Replace data enum value by translated value for datalist
+            var enumsTranslation = enums_radios.translated("ENTITY_NAME", req.session.lang_user, options);
+            var todo = [];
+            for (var i = 0; i < data.data.length; i++) {
+                for (var field in data.data[i].dataValues) {
+                    // Look for enum translation
+                    for (var enumEntity in enumsTranslation)
+                        for (var enumField in enumsTranslation[enumEntity])
+                            if (enumField == field)
+                                for (var j = 0; j < enumsTranslation[enumEntity][enumField].length; j++)
+                                    if (enumsTranslation[enumEntity][enumField][j].value == data.data[i].dataValues[field]) {
+                                        data.data[i].dataValues[field] = enumsTranslation[enumEntity][enumField][j].translation;
+                                        break;
+                                    }
+
+                    //get attribute value
+                    var value = data.data[i].dataValues[field];
+                    //for type picture, get thumbnail picture
+                    if (typeof attributes[field] != 'undefined' && attributes[field].newmipsType == 'picture' && value != null) {
+                        var partOfFile = value.split('-');
+                        if (partOfFile.length > 1) {
+                            //if field value have valide picture name, add new task in todo list
+                            //we will use todo list to get all pictures binary
+                            var thumbnailFolder = global.thumbnail.folder;
+                            var filePath = thumbnailFolder + 'ENTITY_NAME/' + partOfFile[0] + '/' + value;
+                            todo.push({
+                                value: value,
+                                file: filePath,
+                                field: field,
+                                dataIndex: i
+                            });
+                        }
                     }
                 }
             }
-        }
-        //check if we have to get some picture buffer before send data
-        if (todo.length) {
-            var counter=0;
-            for (var i = 0; i < todo.length; i++) {
-                (function (task) {
-                    file_helper.getFileBuffer64(task.file, function (success, buffer) {
-                        counter++;
-                        data.data[task.dataIndex].dataValues[task.field] = {
-                            value: task.value,
-                            buffer: buffer
-                        };
-                        if (counter === todo.length)
-                            res.send(data).end();
+            //check if we have to get some picture buffer before send data
+            if (todo.length) {
+                var counter=0;
+                for (var i = 0; i < todo.length; i++) {
+                    (function (task) {
+                        file_helper.getFileBuffer64(task.file, function (success, buffer) {
+                            counter++;
+                            data.data[task.dataIndex].dataValues[task.field] = {
+                                value: task.value,
+                                buffer: buffer
+                            };
+                            if (counter === todo.length)
+                                res.send(data).end();
 
-                    });
-                }(todo[i]));
-            }
-        } else
-            res.send(data).end();
+                        });
+                    }(todo[i]));
+                }
+            } else
+                res.send(data).end();
+        });
     }).catch(function (err) {
         console.log(err);
         logger.debug(err);
