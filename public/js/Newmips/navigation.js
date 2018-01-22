1112b1a649a5ba02dfaa888d4d7a4a8776f7f370
annelvk <anne.le-van-kiem@newmips.com>
2018-01-22 11:05:17 +0100

    update license reference

--- public/js/Newmips/navigation.js
+++ public/js/Newmips/navigation.js
@@ -1,3 +1,9 @@
+/*!
+ * Newmips v2.5
+ * Copyright 2016
+ * Licensed under GPLV3.0 https://www.gnu.org/licenses/gpl.html
+ */
+
 function navigate(){
   var url = window.document.getElementById('dynamic_select').value; // get selected value
   if (url) { // require a URL
