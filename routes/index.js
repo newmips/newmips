88d9d6e3c2366d13154069bf53dbeaf519fcca08
Syno <stephane.bourdier@newmips.com>
2018-05-30 16:13:25 +0200

    Add new pug file

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var path = require('path');
+var basename = path.basename(module.filename);
+
+module.exports = function(app) {
+	fs.readdirSync(__dirname).filter(function(file){
+		return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+	}).forEach(function(file){
+		file = file.slice(0, -3);
+		if (file === 'routes')
+			app.use('/', require('./'+file));
+		else
+			app.use('/'+file, require('./'+file));
+	});
+}
