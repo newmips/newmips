e3790a632e10ae9844292113bbed9b37a606925b
Syno <stephane.bourdier@newmips.com>
2018-10-19 10:52:56 +0200

    Work in progress

--- config/gitlab.js
+++ config/gitlab.js
@@ -2,13 +2,31 @@ var globalConf = require('./global');
 
 var config = {
     develop: {
-        "doGit": false,
-        "protocol": "http",
-        "url": "cloud.newmips.com:1400",
-        "sshUrl": "git@cloud.newmips.com",
-        "useSSH": true,
-        "adminUser": "",
-        "privateToken": ""
+        doGit: false, // Should the application try to push your app on a repository ?
+        protocol: "http",
+        url: "", // Your gitlab url
+        sshUrl: "", // Something like git@yourgitlaburl
+        useSSH: true, // Todo HTTP non handled for now
+        adminUser: "", // Gitlab admin user
+        privateToken: "" // Gitlab private token
+    },
+    recette: {
+        doGit: false,
+        protocol: "http",
+        url: "",
+        sshUrl: "",
+        useSSH: true,
+        adminUser: "",
+        privateToken: ""
+    },
+    production: {
+        doGit: false,
+        protocol: "http",
+        url: "",
+        sshUrl: "",
+        useSSH: true,
+        adminUser: "",
+        privateToken: ""
     }
 }
 
