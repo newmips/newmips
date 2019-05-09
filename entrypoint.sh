8f43ca03103167e8d2568dd5c27391a283a9f584
Syno <stephane.bourdier@newmips.com>
2019-05-09 09:10:25 +0200

    Merge compile.sh in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,21 @@
 
 sleep 60
 
+#Install Newmips app
+echo "Starting generator nodes modules installation"
+npm install
+
+#Create workspace dir
+if [ ! -d "$workspace" ]; then
+        echo "Create the workspace directory"
+        mkdir workspace
+        chmod 755 -R workspace
+fi
+
+#Install Newmips structure template
+echo "Starting template nodes modules installation"
+cp structure/template/package.json workspace/
+cd workspace
+npm install
+
 npm start
