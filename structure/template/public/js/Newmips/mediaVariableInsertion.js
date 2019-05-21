0757daf8e203e41e3933ed0716d29fb1eaf86d4a
Syno <stephane.bourdier@newmips.com>
2019-05-21 17:16:20 +0200

    Fix select2 input for media creation

--- structure/template/public/js/Newmips/mediaVariableInsertion.js
+++ structure/template/public/js/Newmips/mediaVariableInsertion.js
@@ -9,13 +9,13 @@ var insertionHandler = {
                 url: '/media/entity_tree/'+entity,
                 success: function(entityTree) {
                     /* Create select  and options */
-                    var fieldSelect = '<select style="float:left;" class="fieldInsertion" name="insertionSelect" data-type="field">';
+                    var fieldSelect = '<select class="fieldInsertion" name="insertionSelect" data-type="field">';
                     fieldSelect += '<option value="-1">'+CHOOSE_FIELD+'</option>';
                     for (var i = 0; i < entityTree.length; i++)
                         fieldSelect += '<option value="'+entityTree[i].codename+'">'+entityTree[i].traduction+'</option>';
                     fieldSelect += '</select>';
 
-                    $(fieldSelect).appendTo(label).css('width', '230px').select2();
+                    $(label).after(fieldSelect).next().select2().nextAll('input').first().css("margin-top", "5px");
                 }
             });
         },
@@ -25,9 +25,9 @@ var insertionHandler = {
     },
     group: {
         displaySelector: function(label) {
-            var groupSelect = '<select style="float:left;" class="ajax form-control" data-type="group" name="insertionSelect" data-source="group" data-using="f_label"></select>';
-            $(groupSelect).appendTo(label).css('width', '230px');
-            select2_ajaxsearch(label.find('select'), CHOOSE_GROUP);
+            var groupSelect = '<select class="ajax form-control" data-type="group" name="insertionSelect" data-source="group" data-using="f_label"></select>';
+            $(label).after(groupSelect).nextAll('input').first().css("margin-top", "5px");
+            select2_ajaxsearch(label.next('select'), CHOOSE_GROUP);
         },
         insertValue: function(data) {
             return "{group|"+data.text+"|"+data.id+"}";
@@ -35,9 +35,11 @@ var insertionHandler = {
     },
     user: {
         displaySelector: function(label) {
-            var groupSelect = '<select style="float:left;" class="ajax form-control" data-type="user" name="insertionSelect" data-source="user" data-using="f_login,f_email"></select>';
-            $(groupSelect).appendTo(label).css('width', '230px');
-            select2_ajaxsearch(label.find('select'), CHOOSE_USER);
+            var userSelect = '<select class="ajax form-control" data-type="user" name="insertionSelect" data-source="user" data-using="f_login,f_email"></select>';
+
+            $(label).after(userSelect).nextAll('input').first().css("margin-top", "5px");
+
+            select2_ajaxsearch(label.next('select'), CHOOSE_USER);
         },
         insertValue: function(data) {
             return "{user|"+data.text+"|"+data.id+"}";
@@ -53,13 +55,14 @@ var insertionHandler = {
                 url: '/media/user_tree/'+entity,
                 success: function(userTree) {
                     /* Create select  and options */
-                    var fieldSelect = '<select style="float:left;" class="emailFieldInsertion" name="insertionSelect" data-type="user_target">';
+                    var fieldSelect = '<select class="emailFieldInsertion" name="insertionSelect" data-type="user_target">';
                     fieldSelect += '<option value="-1">'+CHOOSE_USER_TARGET+'</option>';
                     for (var i = 0; i < userTree.length; i++)
                         fieldSelect += '<option value="'+userTree[i].field+'">'+userTree[i].traduction+'</option>';
                     fieldSelect += '</select>';
 
-                    $(fieldSelect).appendTo(label).css('width', '230px').select2();
+                    $(label).after(fieldSelect).next().select2().nextAll('input').first().css("margin-top", "5px");
+                    // $(fieldSelect).appendTo(label).css('width', '230px').select2();
                 }
             });
         },
@@ -123,6 +126,8 @@ var insertionHandler = {
 }
 
 $(function() {
+
+
     // Bind select generation on click
     $(".insert").click(function(e) {
         var type = $(this).data('type');
@@ -130,6 +135,7 @@ $(function() {
 
         // Remove previously created selects
         targetLabel.find('select').eq(0).select2('destroy').remove();
+        targetLabel.nextAll('select').first().select2('destroy').remove();
         // Display new select depending on type
         insertionHandler[type].displaySelector(targetLabel);
     });
@@ -152,7 +158,7 @@ $(function() {
     });
 
     // Insert value in element when option selected
-    $(document).delegate('select[name=insertionSelect]','select2:selecting', function(e) {
+    $(document).on('select2:selecting', 'select[name=insertionSelect]', function(e) {
         var data = e.params.args.data;
         var event = e.params.args.originalEvent;
         var type = $(this).data('type');
