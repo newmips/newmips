1bf1d253e130a780809610633b882233253eaf2f
Magik <boyirou@gmail.com>
2019-07-19 13:24:47 +0200

    - Replace open street map version. Set version 4.6.5 - Update script map script

--- structure/template/public/js/newmips.js
+++ structure/template/public/js/newmips.js
@@ -1209,13 +1209,13 @@ function initComponentAddress(context) {
             $('.address_field').on('keyup', function () {
                 $(this).val($(this).val().toUpperCase());
             });
-            $("#address_search_area", context).each(function () {
+            $("#address_search_input", context).each(function () {
                 var result;
                 var fieldsToShow = componentAddressConf.autocomplete_field.split(',');
                 $(this).autocomplete({
                     minLength: 1,
                     source: function (req, res) {
-                        var val = $('#address_search_area').val();
+                        var val = $('#address_search_input').val();
                         var data = {limit: 10};
                         data[componentAddressConf.query_parm] = val;
                         $.ajax({
@@ -1378,32 +1378,77 @@ function initMapsIfComponentAddressExists(context) {
             });
         }
     });
+   
     function initComponentAddressMaps(lat, lon, mapsContext) {
         try {
             $(mapsContext).find('.address_maps').each(function () {
+                var that = $(this);
                 $(this).empty();
+                var control = ol.control.defaults();
                 var options = {
                     controls: []
                 };
-                if ($('.f_address_navigation', mapsContext).val() === 'true')
-                    options.controls.push(new OpenLayers.Control.Navigation());
-                if ($('.f_address_zoomBar', mapsContext).val() === 'true')
-                    options.controls.push(new OpenLayers.Control.PanZoomBar());
-                if ($('.f_address_mousePosition', mapsContext).val() === 'true')
-                    options.controls.push(new OpenLayers.Control.MousePosition());
+                lon = parseFloat(lon);
+                lat = parseFloat(lat);
 
-                var map = new OpenLayers.Map($(this).attr('mapsid'), options);
-                var mapnik = new OpenLayers.Layer.OSM();
-                var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
-                var toProjection = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
-                var position = new OpenLayers.LonLat(lon, lat).transform(fromProjection, toProjection);
-                var zoom = 15;
-                var markers = new OpenLayers.Layer.Markers("Markers");
+                const markerSource = new ol.source.Vector();
+                var markerStyle = new ol.style.Style({
+                    image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+                        anchor: [0.5, 46],
+                        anchorXUnits: 'fraction',
+                        anchorYUnits: 'pixels',
+                        opacity: 0.75,
+                        src: '../img/address_map_marker.png'
+                    }))
+                });
+                var iconFeature = new ol.Feature({
+                    geometry: new ol.geom.Point(ol.proj.transform([lon, lat], 'EPSG:4326',
+                            'EPSG:3857')),
+                    name: '',
+                    population: 4000,
+                    rainfall: 500
+                });
 
-                map.addLayer(markers);
-                markers.addMarker(new OpenLayers.Marker(position));
-                map.addLayer(mapnik);
-                map.setCenter(position, zoom);
+                markerSource.addFeature(iconFeature);
+                if ($('.f_address_zoomBar', mapsContext).val() === 'true') {
+                    var zoomSlider = new ol.control.ZoomSlider();
+                    options.controls.push(zoomSlider)
+                }
+                if ($('.f_address_mousePosition', mapsContext).val() === 'true') {
+                    var mousePositionControl = new ol.control.MousePosition({
+                        coordinateFormat: ol.coordinate.createStringXY(4),
+                        projection: 'EPSG:4326',
+                        // comment the following two lines to have the mouse position
+                        // be placed within the map.
+                        className: 'custom-mouse-position',
+//                        target: document.getElementById('mouse-position'),
+                        undefinedHTML: '&nbsp;'
+                    });
+                    options.controls.push(mousePositionControl);
+                }
+                var mapConfig = {
+                    controls: control.extend(options.controls),
+                    target: that.attr('id'),
+                    layers: [
+                        new ol.layer.Tile({
+                            source: new ol.source.OSM()
+                        }),
+                        new ol.layer.Vector({
+                            source: markerSource,
+                            style: markerStyle,
+                        })
+                    ],
+                    view: new ol.View({
+                        center: ol.proj.fromLonLat([lon, lat]),
+                        zoom: 17
+                    })
+                };
+                if ($('.f_address_navigation', mapsContext).val() === 'false') {
+                    mapConfig.interactions = [];
+                    mapConfig.controls = [];
+                }
+                var map = new ol.Map(mapConfig);
+
             });
         } catch (e) {
             console.log(e);
