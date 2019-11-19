fca2509ffcb471cc6d894dfd9cfbe290a0adeff8
Syno <stephane.bourdier@newmips.com>
2019-11-19 09:51:54 +0100

    Eslint generator routes ok

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ module.exports = app => {
 	fs.readdirSync(__dirname).filter(file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js').forEach(function(file){
 		file = file.slice(0, -3);
 		if (file === 'routes')
-			app.use('/', require('./'+file));
+			app.use('/', require('./'+file)); // eslint-disable-line
 		else
-			app.use('/'+file, require('./'+file));
+			app.use('/'+file, require('./'+file)); // eslint-disable-line
 	});
 }
