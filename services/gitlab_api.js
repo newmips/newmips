e1f402caf2ef29270bbe7bc6f26e4e95f61c5087
Syno <stephane.bourdier@newmips.com>
2019-11-22 18:15:13 +0100

    Cloud env improvments

--- services/gitlab_api.js
+++ services/gitlab_api.js
@@ -126,6 +126,9 @@ exports.getAllProjects = async () => {
 			'Content-Type': 'application/json',
 			'Private-Token': token
 		},
+		qs: {
+			per_page: 100 // Max limit
+		},
 		json: true // Automatically stringifies the body to JSON
 	};
 
@@ -147,9 +150,6 @@ exports.getProjectByID = async (projectID) => {
 			'Content-Type': 'application/json',
 			'Private-Token': token
 		},
-		qs: {
-			per_page: 100
-		},
 		json: true // Automatically stringifies the body to JSON
 	};
 
@@ -169,12 +169,13 @@ exports.getProjectByName = async (projectName) => {
             'Private-Token': token
         },
         qs: {
-            search: globalConf.host // Reduce search on current generator repository
+            search: globalConf.host, // Reduce search on current generator repository
+            per_page: 100 // Max limit
         },
         json: true // Automatically stringifies the body to JSON
     };
 
-    console.log("GITLAB CALL => getProjectByName");
+    console.log("GITLAB CALL => getProjectByName => " + projectName);
 
     try {
     	let allProjects = await request(options);
