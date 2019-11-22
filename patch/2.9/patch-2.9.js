e2f47936b85a865f6f465c170217f4696416c9d4
Syno <stephane.bourdier@newmips.com>
2019-11-22 17:13:52 +0100

    Fix patch 2.8.js

--- patch/2.9/patch-2.9.js
+++ patch/2.9/patch-2.9.js
@@ -2,24 +2,37 @@ console.log('Executing 2.9 js patch...');
 const fs = require('fs-extra');
 const models = require('../../models/')
 const workspacePath = __dirname + '/../../workspace/';
+const dataHelper = require('../../utils/data_helper');
 
 (async () => {
-	try {
-		// Executing SQL file
-		await models.sequelize.query('ALTER TABLE application DROP COLUMN name;');
-		await models.sequelize.query('ALTER TABLE application CHANGE codeName name varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;');
-	} catch(err) {
-		console.error(err);
-	}
-})().then(_ => {
-	const promises = [];
 
+	const queries = [
+		'ALTER TABLE application DROP COLUMN name;',
+		'ALTER TABLE application DROP COLUMN codeName;',
+		'ALTER TABLE application ADD COLUMN name varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;'
+	]
+
+	// Executing SQL file
+	for (var i = 0; i < queries.length; i++) {
+		try {
+			await models.sequelize.query(queries[i]);
+		} catch(err) {
+			console.error("Error occured while executing SQL patch: " + queries[i]);
+		}
+	}
+
+	const promises = [];
 	fs.readdirSync(workspacePath).filter(x => !isNaN(x) && fs.lstatSync(workspacePath + x).isDirectory()).forEach((folder) => {
 		promises.push((async () => {
 
 			// Getting appName
-			const {name} = await models.Application.findByPk(folder);
-			console.log('Patching application: ' + name + '(' + folder + ')');
+			const app = await models.Application.findByPk(folder);
+			const name = 'a_' + dataHelper.clearString(app.displayName);
+			console.log('Patching application: ' + name + ' (' + folder + ')');
+
+			await app.update({
+				name: name
+			});
 
 			const newWorkspacePath = workspacePath + name;
 			fs.renameSync(workspacePath + folder, newWorkspacePath);
@@ -77,12 +90,9 @@ const workspacePath = __dirname + '/../../workspace/';
 		})())
 	});
 
-	Promise.all(promises).then(_ => {
-		console.log('2.8 js patch done.');
-		process.exit(1);
-	});
+	await Promise.all(promises);
 
-}).catch(err => {
-	console.error(err);
+})().then(_ => {
+	console.log('2.9 js patch done.');
 	process.exit(1);
-});
+})
