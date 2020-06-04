867b27e198df0db442361477eeab6d26443ab2f2
Syno <stephane.bourdier@newmips.com>
2020-06-04 14:58:12 +0200

    Handling enum translation in level 1 association in datalist

--- structure/template/utils/enum_radio.js
+++ structure/template/utils/enum_radio.js
@@ -49,28 +49,43 @@ exports.translated = function (entity, lang, options) {
 // Looking for using field in row entity association to translate it based on locales/enum_radio.json
 // Useful in related to field that using an enum or radio field
 exports.translateUsingField = function (entity, options, translate, forDatalist = false) {
+	let translations, aliasRow, field, translateValue;
 	for (let i = 0; i < options.length; i++) {
-		const target = translate[options[i].target];
-		const aliasRow = entity[options[i].as];
-		if(target && aliasRow && options[i].usingField && options[i].usingField.length > 0) {
+		translations = translate[options[i].target];
+		aliasRow = entity[options[i].as];
+
+		if(!translations || !aliasRow)
+			continue;
+
+		if(options[i].usingField && options[i].usingField.length > 0) {
+			// Related to case
 			for (let j = 0; j < options[i].usingField.length; j++) {
-				const field = options[i].usingField[j].value;
+				field = options[i].usingField[j].value;
 
 				// Not a field that need translation (not in locales)
-				if(!target[field])
+				if(!translations[field])
 					continue;
 
 				if(Array.isArray(aliasRow)) {
 					// Related to many
 					for (let k = 0; k < aliasRow.length; k++) {
-						const translateValue = target[field].filter(x => x.value == aliasRow[k][field])[0];
+						translateValue = translations[field].filter(x => x.value == aliasRow[k][field])[0];
 						aliasRow[k][field] = forDatalist ? translateValue.translation : translateValue;
 					}
 				} else {
 					// Related to
-					const translateValue = target[field].filter(x => x.value == aliasRow[field])[0];
+					translateValue = translations[field].filter(x => x.value == aliasRow[field])[0];
 					aliasRow[field] = forDatalist ? translateValue.translation : translateValue;
 				}
+			}
+		} else {
+			// Enum in has one case
+			for (const fieldTranslated in translations) {
+				if(!aliasRow[fieldTranslated])
+					continue;
+
+				translateValue = translations[fieldTranslated].filter(x => x.value == aliasRow[fieldTranslated])[0];
+				aliasRow[fieldTranslated] = forDatalist ? translateValue.translation : translateValue;
 			}
 		}
 	}
