1d2d9c75d0ddbd787c18645e152ae514d40b25fe
FLafaye <40290133+FLafaye@users.noreply.github.com>
2020-06-11 11:49:01 +0200

    fix display update button on initHasOne function

--- structure/template/public/js/Newmips/show.js
+++ structure/template/public/js/Newmips/show.js
@@ -196,11 +196,11 @@ function initHasOne(tab, data) {
         delForm.attr('action', '/'+data.option.target.substring(2)+'/delete');
         delForm.find('input[name=id]').val(data.data);
 
+		newButton = $('<div class="quicklinks"></div>');
+
         // Add update btn
         if(data.option.access.update)
-            delForm.prepend(updBtn);
-
-        newButton = $('<div class="quicklinks"></div>');
+            newButton.prepend(updBtn);
 
         // Add delete btn
         if(data.option.access.delete)
