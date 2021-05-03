11cd5862deee8562f9407b2bc34b3372860f762b
stephane.bourdier@newmips.com <stephane.bourdier@newmips.com>
2021-05-03 09:26:50 +0200

    Revert "Config"
    This reverts commit 9435ce360f2641baf5593ed4b1be73c489152bb9.

---
+++ config/portainer.js
@@ -0,0 +1,16 @@
+const globalConf = require('./global');
+
+const config = {
+	docker: {
+		url: "",
+		login: "",
+		password: ""
+	},
+	cloud: {
+		url: "",
+		login: "",
+		password: ""
+	}
+}
+
+module.exports = config[globalConf.env];
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,9 @@ git config --global user.name "$HOSTNAME"
 git config --global user.email "$SUB_DOMAIN@$DOMAIN_STUDIO"
 
 # Write SSH Config file
-printf "Host gitlab.%s\n	Port 22\n	StrictHostKeyChecking no\n" "$DOMAIN_STUDIO" > /root/.ssh/config
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
+# printf "Host gitlab.%s\n	Port 22\n	StrictHostKeyChecking no\n" "$DOMAIN_STUDIO" > /root/.ssh/config
+# eval "$(ssh-agent -s)"
+# ssh-add ~/.ssh/id_rsa
 
 # Install generator and application node_modules
 echo "Installing generator and app node modules..."
